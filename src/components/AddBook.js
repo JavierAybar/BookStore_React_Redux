2b76badfd2b4a17b20e34045e59f3194c2740378
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-08-22 15:18:38 -0300

    add reducers and actions

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -6,7 +6,7 @@ function AddBook() {
       <h2 className="addBook__title">ADD NEW BOOK</h2>
       <div className="container__inputs">
         <input className="addBook__input" type="text" placeholder="Book Title" />
-        <select className="addBook__select" name="" id="">
+        <select className="addBook__select" name="" id="1">
           <option value="Action">Action</option>
         </select>
         <button className="addBook__btn" type="submit">ADD BOOK</button>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,37 @@
 import Book from './Book';
 
 function BookList() {
+  const books = [
+    {
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ];
+
   return (
     <div className="books">
-      <Book title="The Hunger Games" author="Suzanne Collins" />
-      <Book title="Another Book Title" author="Another Author" />
-      <Book title="Yet Another Title" author="Yet Another Author" />
+      {books.map((item) => (
+        <Book
+          key={item.id}
+          title={item.title}
+          author={item.author}
+          category={item.category}
+        />
+      ))}
     </div>
   );
 }
